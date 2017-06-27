5dfe1bb524e1a689b90394fa4f922818f3522448
MainShayne233 <shaynetremblay@hotmail.com>
2017-06-26 22:16:02 -0400

    init

---
+++ lib/phx_app.ex
@@ -0,0 +1,18 @@
+defmodule PhxApp do
+  @moduledoc """
+  Documentation for PhxApp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxApp.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_app_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxAppTest do
+  use ExUnit.Case
+  doctest PhxApp
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
